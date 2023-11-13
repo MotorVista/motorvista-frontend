e1095152bf356306c81adb16ae79964766362d29
AmyresS <amyresslol@gmail.com>
2023-11-13 01:50:28 +0100

    added css for image blocks

--- src/pages/CarPage.tsx
+++ src/pages/CarPage.tsx
@@ -8,10 +8,20 @@ function CarPage (){
                 <div id="light"></div>
                 <div id="vignette"></div>
                 <div className="content">
-                    <img src="../assets/images/grsport_ico.png" alt="grsport ico" />
+                    <img id="icon" src="../assets/images/icons/grsport_ico.png" alt="grsport ico" />
+                    <img id="car" src="../assets/images/car_model.png" alt="car model" />
                 </div>
             </div>
-            <div className="imageBlock"></div>
+            <div className="imageBlock">
+                <div className="imageContainer">
+                    <img src="../assets/images/engine_view.png" alt="engine model" />
+                </div>
+                <div className="header">
+                    <img src="../assets/images/icons/engine_ico.png" alt="engine icon" id="icon" />
+                    <div className="headerText">Engine</div>
+                    <div className="headerDesc">B46B2001</div>
+                </div>
+            </div>
             <div className="infoBlock"></div>
             <div className="imageBlock"></div>
             <div className="infoBlock"></div>
