965466e70b645d55c498527ef39fc30b08d74a0d
AmyresS <amyresslol@gmail.com>
2023-11-23 12:01:33 +0100

    Added the main page and completed the car page by 90%

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,7 +1,18 @@
 function NavMenu () {
     return(
         <>
-            <div id="menuBar"></div>
+            <div id="menuBar">
+                <div className="left">
+                    <a href="">Back</a>
+                    <a href="/">Main page</a>
+                </div>
+                <div className="center">
+                    <a href=""><img src="" alt="Toyota" /></a>
+                </div>
+                <div className="right">
+                    <a href="">Profile</a>
+                </div>
+            </div>
         </>
     )
 }
