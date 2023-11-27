42c57404a20815596ad0c245104deda1f7186073
mykola2312 <nikolay_tihonov@i.ua>
2023-11-27 15:10:42 +0200

    add axios API instance

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosResponse } from "axios";
+
+export const API_PATH = "http://localhost:3000";
+
+export const API = axios.create({
+    baseURL: API_PATH
+});
+
+export function checkResponse(res: AxiosResponse): boolean {
+    return !res.data?.error;
+}
